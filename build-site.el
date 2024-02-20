c816c725b775e67d286fe34d762210b5940cfcc3
christoph <christoph@schottmueller.me>
2024-02-20 09:17:25 +0100

    throw in kitchen sink

--- build-site.el
+++ build-site.el
@@ -1,10 +1,30 @@
-(require 'package)
-(setq package-user-dir (expand-file-name "./.packages"))
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;;; ----------------------------------------------------------------------------
+;;; package system configuration
+;;; ----------------------------------------------------------------------------
 
-;; Initialize the package system
+(require 'package)
+
+(setq package-enable-at-startup nil)
+
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
+
+;; bootstrap use-package
+(setq package-pinned-packages
+      '((bind-key    . "melpa-stable")
+        (diminish    . "melpa-stable")
+        (use-package . "melpa-stable")))
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure t)
+
+(use-package org :pin gnu)
 
 (unless (package-installed-p 'auto-package-update)
   (package-refresh-contents)
